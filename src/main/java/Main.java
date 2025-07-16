7722c9c391d4f40c7c44b21c5f9e4477bf92e311
Berk <tiritberk@gmail.com>
2025-07-16 13:25:33 +0300

    body mass calculator

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        double height = input.nextDouble();
+
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        double weight = input.nextDouble();
+
+        double bodyMass = weight / (height * height);
+        System.out.println("Vücut Kilte İndeksiniz: " + bodyMass);
+    }
+}
