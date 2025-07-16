e12bba93773b0d676f355ff2ad23ac2a2f8b6efd
Berk <tiritberk@gmail.com>
2025-07-16 15:43:14 +0300

    practices

---
+++ src/main/java/ChineseZodiac.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ChineseZodiac {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        int birthYear = input.nextInt();
+        int zodiac = birthYear % 12;
+
+        String zodiacSign = switch (zodiac) {
+            case 0 -> "Maymun";
+            case 1 -> "Horoz";
+            case 2 -> "Köpek";
+            case 3 -> "Domuz";
+            case 4 -> "Fare";
+            case 5 -> "Öküz";
+            case 6 -> "Kaplan";
+            case 7 -> "Tavşan";
+            case 8 -> "Ejderha";
+            case 9 -> "Yılan";
+            case 10 -> "At";
+            case 11 -> "Koyun";
+            default -> "Bilinmeyen";
+        };
+
+        System.out.println("Çin Zodyağı Burcunuz : " + zodiacSign);
+        input.close();
+    }
+}
