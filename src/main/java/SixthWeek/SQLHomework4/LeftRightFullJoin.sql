291edbed00dac3806cfe10883ae00d987f4f858a
Berk <tiritberk@gmail.com>
2025-08-21 18:25:50 +0300

    practices

---
+++ src/main/java/SixthWeek/SQLHomework4/LeftRightFullJoin.sql
@@ -0,0 +1,14 @@
+-- 1. city ile country tabloları (LEFT JOIN)
+SELECT city.city, country.country
+FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+
+-- 2. customer ile payment tabloları (RIGHT JOIN)
+SELECT customer.first_name, customer.last_name, payment.payment_id
+FROM customer
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
+
+-- 3. customer ile rental tabloları (FULL JOIN)
+SELECT customer.first_name, customer.last_name, rental.rental_id
+FROM customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
